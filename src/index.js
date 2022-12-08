d4113cdf6ba6d5b0e1b87439c5d5eb6ff8de7d6e
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-12-08 17:37:02 +0100

    init

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/wallet/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
